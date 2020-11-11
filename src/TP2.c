dd6ec4131d13d423d8ce06737e4bd4081d3d1c96
Alain <alainjosevz@gmail.com>
2020-11-10 22:33:55 -0300

    Se creo el file entrada.h

--- src/TP2.c
+++ src/TP2.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "entrada.h"
 
 int main(void) {
 	puts("¿5+6?");
---
+++ src/entrada.h
@@ -0,0 +1,28 @@
+/*
+ * entrada.h
+ *
+ *  Created on: 10 nov. 2020
+ *      Author: lp1-2019
+ */
+
+#ifndef ENTRADA_H_
+#define ENTRADA_H_
+
+// valores de distancias
+#define PUNTOS 9
+
+int distancias[PUNTOS][PUNTOS] =
+ {{0,15,13,5,0,0,0,0,0},
+ {0,0,0,0,0,0,0,12,0},
+ {0,2,0,18,0,6,0,0,0},
+ {0,0,0,0,4,0,0,0,99},
+ {0,0,3,0,0,1,9,0,14},
+ {0,8,0,0,0,0,0,17,0},
+ {0,0,0,0,0,16,0,7,10},
+ {0,0,0,0,0,0,0,0,0},
+ {0,0,0,0,0,0,0,11,0}};
+
+char x = 'A'; // punto origen
+char y = 'I'; // punto destino
+
+#endif /* ENTRADA_H_ */

c68d7029abdc0505cfb63879eaac83191846bf01
Alain <alainjosevz@gmail.com>
2020-11-10 22:00:54 -0300

    Se inicia el proyecto

---
+++ src/TP2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP2.c
+ Author      : Alain Vega y Mathias Martinez
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("¿5+6?");
+	return EXIT_SUCCESS;
+}
